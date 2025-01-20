900abfcc3276c78ee6d46fc58302f3c716c3a055
Flw-me <Kubatiev.r@gmail.com>
2025-01-20 15:43:39 +0300

    Add files via upload

---
+++ ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dota2Stats
+{
+    public class ApiResponse<T>
+    {
+        [JsonProperty("rows")]
+        public List<T> Rows { get; set; }
+    }
+}
